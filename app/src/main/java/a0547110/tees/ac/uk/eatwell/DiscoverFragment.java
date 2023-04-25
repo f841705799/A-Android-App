f8d01dd49cdb5d525b3ac05b8e47479ee19a07aa
weiwudi <wwd.fj@live.cn>
2023-04-25 02:22:10 +0100

    Add DiscoverFragment part function code.

--- app/src/main/java/a0547110/tees/ac/uk/eatwell/DiscoverFragment.java
+++ app/src/main/java/a0547110/tees/ac/uk/eatwell/DiscoverFragment.java
@@ -1,19 +1,40 @@
 package a0547110.tees.ac.uk.eatwell;
 
+
+
+import android.annotation.SuppressLint;
+
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.Manifest;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,12 +48,15 @@ public class DiscoverFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private RecyclerView mRecyclerView;
     private ShopAdapter ShopAdapter;
     private List<Shop> list = new ArrayList<>();
+    private FusedLocationProviderClient fusedLocationClient;
+    private ProgressBar progressBar;
+    private String latitude;
+    private String longitude;
+
+
 
     public DiscoverFragment() {
         // Required empty public constructor
@@ -56,19 +80,74 @@ public class DiscoverFragment extends Fragment {
         return fragment;
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
+        // Initialize the SDK
+        Places.initialize(getApplicationContext(), BuildConfig.MAPS_API_KEY);
 
-    }
+        // Create a new PlacesClient instance
+        PlacesClient placesClient = Places.createClient(getContext());
+        */
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = null;
+                            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                                fineLocationGranted = result.getOrDefault(
+                                        Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            }
+                            Boolean coarseLocationGranted = null;
+                            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                                coarseLocationGranted = result.getOrDefault(
+                                        Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            }
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                            } else {
+                                // No location access granted.
+                                Log.d("DiscoverFragment", "No location access granted.");
+                            }
+                        }
+                );
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+
+        }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        initData();
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
         mRecyclerView = view.findViewById (R.id.reclcler_view);
+        progressBar = view.findViewById(R.id.progressBar);
+        // Inflate the layout for this fragment
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+                .addOnSuccessListener(currentlocation -> {
+                    // Got last known location. In some rare situations this can be null.
+                    if (currentlocation != null) {
+                        latitude = String.valueOf(currentlocation.getLatitude());
+                        longitude = String.valueOf(currentlocation.getLongitude());
+                        Log.d("DiscoverFragment", "onCreate: " + currentlocation.toString());
+                        initData();
+                    }
+                    else {
+                        Log.d("DiscoverFragment", "onCreate: location is null");
+                    }
+                });
+        mRecyclerView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
         ShopAdapter = new ShopAdapter (getActivity (),list);
         mRecyclerView.setLayoutManager (new LinearLayoutManager (getActivity (), LinearLayoutManager.VERTICAL,false));
         mRecyclerView.setItemAnimator (new DefaultItemAnimator());
@@ -76,10 +155,37 @@ public class DiscoverFragment extends Fragment {
         mRecyclerView.addItemDecoration (new DividerItemDecoration (getActivity (), DividerItemDecoration.VERTICAL));
         return view;
     }
+
+
     private void initData(){
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+latitude+"%2C"+longitude+"&radius=1500&type=restaurant&keyword=&key="+BuildConfig.MAPS_API_KEY;
+        Log.d("DiscoverFragment", "initData: " + url);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("DiscoverFragment", "onResponse: " + response);
+                    }
+                },
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
         for (int i = 0; i < 10; i++) {
             Shop shop = new Shop(Integer.toString(i));
             list.add (shop);
         }
+        ShopAdapter.notifyDataSetChanged();
+        progressBar.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
     }
 }
